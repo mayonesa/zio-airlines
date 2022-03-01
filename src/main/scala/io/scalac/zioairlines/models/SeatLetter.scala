85b087abb830750f2fde5c6b13ddaadc552b1802
juancho <juanchoj1024@gmail.com>
2022-03-01 13:49:16 -0500

    NPE

--- src/main/scala/io/scalac/zioairlines/models/SeatLetter.scala
+++ src/main/scala/io/scalac/zioairlines/models/SeatLetter.scala
@@ -1,6 +1,4 @@
 package io.scalac.zioairlines.models
 
 enum SeatLetter:
-  val NRows: Int = SeatLetter.values.length
-
   case A, B, C, D
--- src/main/scala/io/scalac/zioairlines/models/SeatRow.scala
+++ src/main/scala/io/scalac/zioairlines/models/SeatRow.scala
@@ -3,6 +3,4 @@ package io.scalac.zioairlines.models
 import io.scalac.zioairlines.adts.Index
 
 enum SeatRow:
-  val NRows: Int = SeatRow.values.length
-
   case `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`
