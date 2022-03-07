3c893092b9ac26db6ed8d7e810723d7d7db6a1ac
juancho <juanchoj1024@gmail.com>
2022-03-07 09:20:49 -0500

    code by contract

--- src/main/scala/io/scalac/zioairlines/adts/IncrementingKeyMap.scala
+++ src/main/scala/io/scalac/zioairlines/adts/IncrementingKeyMap.scala
@@ -1,15 +1,21 @@
 package io.scalac.zioairlines.adts
 
-class IncrementingKeyMap[A] private (map: Map[Int, A], latestKey: Int):
-  def add(a: A): IncrementingKeyMap[A] =
-    val k = nextKey
-    IncrementingKeyMap(map + (k -> a), k)
-    
-  def updated(k: Int, a: A): IncrementingKeyMap[A] = IncrementingKeyMap(map.updated(k, a), latestKey)
-
-  def nextKey: Int = latestKey + 1
-
-  def get(k: Int): Option[A] = map.get(k)
+trait IncrementingKeyMap[A]:
+  def add(a: A): IncrementingKeyMap[A]
+  def updated(k: Int, a: A): IncrementingKeyMap[A]
+  def nextKey: Int
+  def get(k: Int): Option[A]
 
 object IncrementingKeyMap:
-  def empty[A]: IncrementingKeyMap[A] = IncrementingKeyMap(Map.empty, 0)
+  def empty[A]: IncrementingKeyMap[A] = new IncrementingKeyMapImpl(Map.empty, 0)
+  
+  private class IncrementingKeyMapImpl[A](map: Map[Int, A], latestKey: Int) extends IncrementingKeyMap[A]:
+    override def add(a: A): IncrementingKeyMap[A] =
+      val k = nextKey
+      IncrementingKeyMapImpl(map + (k -> a), k)
+
+    override def updated(k: Int, a: A): IncrementingKeyMap[A] = new IncrementingKeyMapImpl(map.updated(k, a), latestKey)
+
+    override def nextKey: Int = latestKey + 1
+
+    override def get(k: Int): Option[A] = map.get(k)
