bab50131637eaf9622a98750b32fdaa8fcca9727
juancho <juanchoj1024@gmail.com>
2022-03-19 01:14:47 -0400

    warning on expiration status.

--- src/main/scala/io/scalac/zioairlines/models/booking/Booking.scala
+++ src/main/scala/io/scalac/zioairlines/models/booking/Booking.scala
@@ -28,6 +28,9 @@ private case class BookingImpl(
   override val bookingNumber  : BookingNumber,
   override val status         : BookingStatus = BookingStatus.Started,
   override val seatAssignments: Set[SeatAssignment] = Set(),
+
+  // will cause status to not reflect in the case of expiration unless another action comes in between update and status
+  // look-up. However, there's no outside-package exposure at the time.
   bookingDeadline             : Deadline = BookingTimeLimit.fromNow,
 ) extends Booking:
   override private[booking] def seatsAssigned(
